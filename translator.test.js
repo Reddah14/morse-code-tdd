cff6b20151127ca150ac986b85257ae2e9e97163
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-06 17:40:01 +0200

    test for translating vowels [ 'a' ]

--- translator.test.js
+++ translator.test.js
@@ -2,4 +2,16 @@ import * as morseCodeTranslator from './translator.js'
 
 test('returns full name', () => {
   expect(getFullName("salva", "martinez")).toBe("salva martinez");
-});
+});describe("translateVowelsIntoMorseCode() test", () => {
+
+  test("Function Should return '.-' for 'a' vowel ", () => {
+
+    // 1. Arrange (set my test)
+    const input = "a";
+    
+    // 2. Act (do something)
+    const output = ".-";
+    
+    // 3. Assert (do something that will end up being truth)
+    expect(morseCodeTranslator.translateVowelsIntoMorseCode(input)).toBe(output);
+  })
