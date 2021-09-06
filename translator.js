670cdabd11c5a458d0740f314c9ae7cc4ddf1e58
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-06 17:40:39 +0200

    function for translating vowels [ 'a' ]

--- translator.js
+++ translator.js
@@ -1,3 +1,8 @@
-export const getFullName = (fName, lName) => {
-  return fName + " " + lName;
+export const translateVowelsIntoMorseCode = (vowel) => {
+  if ( vowel === "a" ) {
+    
+    return ".-";
+  } else {
+    return console.log("I can't translate that ! 😓");
+  }
 }
