ff086b08b668c4456430b25f8d9c61b8e589f4de
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-15 20:18:16 +0200

    refactor func to translate

--- translator.js
+++ translator.js
@@ -5,17 +5,18 @@ let translatedString = "";
 const morseCodeKeys = Object.keys(morseCode);
 const morseCodeValues = Object.values(morseCode);
 
-export const translateWordToMorse = (input) => {
-    translatedString = "";
+export const translateStringToMorse = (input) => {
+  translatedString = "";
+  const inputToLowerCase = input.toLowerCase();
+  for (let index = 0; index < inputToLowerCase.length; index++) {
+    const element = inputToLowerCase[index];
 
-    for ( let index = 0; index < input.length; index++ ) {
-        const element = input[index];
-
-        for ( let index = 0; index < morseCodeKeys.length; index++ ) {
-            if ( element === morseCodeKeys[index] ) {
-                translatedString += `${morseCodeValues[index]} ` ;
-            }
-        }
+    for (let index = 0; index < morseCodeKeys.length; index++) {
+      if (element === morseCodeKeys[index]) {
+        translatedString += `${morseCodeValues[index]} `;
+      }
     }
-    return translatedString;
+  }
+  
+  return translatedString;
 };
