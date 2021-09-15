ef1469e222da7f1c20926d3eabe7edf970849a72
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-15 15:53:46 +0200

    adding space after character

--- translator.js
+++ translator.js
@@ -6,12 +6,14 @@ const morseCodeKeys = Object.keys(morseCode);
 const morseCodeValues = Object.values(morseCode);
 
 export const translateWordToMorse = (input) => {
+    translatedString = "";
+
     for ( let index = 0; index < input.length; index++ ) {
         const element = input[index];
 
         for ( let index = 0; index < morseCodeKeys.length; index++ ) {
             if ( element === morseCodeKeys[index] ) {
-                translatedString += morseCodeValues[index];
+                translatedString += `${morseCodeValues[index]} ` ;
             }
         }
     }
