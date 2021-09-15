1c505bae3d2ade644c55ec49d43d3cc1a70ae8cd
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-15 21:28:13 +0200

    adding validations to input

--- translator.js
+++ translator.js
@@ -6,7 +6,15 @@ const morseCodeKeys = Object.keys(morseCode);
 const morseCodeValues = Object.values(morseCode);
 
 export const translateStringToMorse = (input) => {
-  translatedString = "";
+  let translatedString = "";
+  
+  if (input === null ) {
+    return undefined;
+  }
+
+  if (input.length === 0) {
+    return undefined;
+  }
   const inputToLowerCase = input.toLowerCase();
   for (let index = 0; index < inputToLowerCase.length; index++) {
     const element = inputToLowerCase[index];
