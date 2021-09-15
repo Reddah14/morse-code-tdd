dfd72944ac0fc5d5a52bb68d8d84ce2dffbfa77a
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-15 15:53:59 +0200

    fix test to pass after refactor

--- translator.test.js
+++ translator.test.js
@@ -3,13 +3,10 @@ import { translateWordToMorse } from "./translator.js";
 describe("TranslateMorseCode() test", () => {
   
   test("Function test", () => {
-    // 1. Arrange (set my test)
-    const input = "a";
-
-    // 2. Act (do something)
-    const output = ".-";
-
-    // 3. Assert (do something that will end up being truth)
-    expect(translateWordToMorse(input)).toBe(output);
+    expect(translateWordToMorse("a")).toBe(".- ");
   })
+
+  test("Function test1", () => {
+    expect(translateWordToMorse("sos")).toBe("... --- ... ");
+  })  
 });
